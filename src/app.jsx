2b340a8f1224bf4ae0e8d438ea554a0004a5a869
junhp7 <junhp1994@gmail.com>
2021-10-19 16:08:32 +0900

    layout & add search function

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDQqh-xDpOFuFVTCGKFVoyGJSGHIT7p44M`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(newItem => ({...newItem , id: newItem.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
 
   useEffect(() => {
     const requestOptions = {
@@ -16,7 +29,13 @@ function App() {
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   }, []);
-  return <VideoList videos={videos} />
+
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />  
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
