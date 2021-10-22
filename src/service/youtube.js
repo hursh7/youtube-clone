0363a61dc69f70bab1c46f0f11fe03d7cf66907f
junhp7 <junhp1994@gmail.com>
2021-10-22 16:07:34 +0900

    add regionCode

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -8,13 +8,13 @@ class Youtube {
     }
 
     async mostPopular() {
-        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`, this.getRequestOptions);
+        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&regionCode=kr&key=${this.key}`, this.getRequestOptions);
         const result = await response.json();
         return result.items;
     }
 
     async search(query) {
-        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`, this.getRequestOptions);
+        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&regionCode=kr&key=${this.key}`, this.getRequestOptions);
         const result = await response.json();
         return result.items.map(newItem => ({ ...newItem, id: newItem.id.videoId }));
     }
